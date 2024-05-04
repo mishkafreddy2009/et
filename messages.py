6f1f3465cacd83d4ec2ea4f338d1e22b57b25efe
Timofey Basuev <echiauta@gmail.com>
2024-05-04 16:58:11 +0500

    feat: add categories keyword arg in stats function

--- messages.py
+++ messages.py
@@ -1,21 +1,23 @@
-GREETING = """бот для введения личных расходов
+GREETING = """бот для ведения личных расходов
 
 команды:
-/start - приветственное сообщение
-/stats - статистика по расходам
-/add <сумма> - добавить строку расходов
-/total - статистика по расходам за все время
-/week - статистика по расходам за неделю
+*/start* - приветственное сообщение
+*/stats* - статистика по расходам
+*/add* - добавить строку расходов, пример: "/add 500 б" (добавить 500 рублей в категорию бензин)
+*/categories* - посмотреть доступные категории
 """
 
-
-def get_stats(today_spending=0, total_spending=0) -> str:
-    return f"""статистика
+def get_stats(today_spending: int=0, total_spending: int=0, **category_spendings):
+    msg = f"""статистика
 
 расходы за сегодня
 *{today_spending} руб*
 
 расходы за все время
 *{total_spending} руб*
-"""
 
+расходы по категориям
+"""
+    for c, s in category_spendings.items():
+        msg += f"{c} - *{s} руб*\n"
+    return msg
